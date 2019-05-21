3028d62c27c29706623986fb6ba2ae05153944ff
khuong <nbkhuong97@gmail.com>
2019-05-21 21:33:16 +0700

    fix a bit

--- train.py
+++ train.py
@@ -30,8 +30,8 @@ def log_print(text, color=None, on_color=None, attrs=None):
 
 def main():
     # define output folder
-    output_dir = './test_saved_models/'
-    log_dir = './test_mae_mse/'
+    output_dir = './saved_models/'
+    log_dir = './mae_mse/'
     checkpoint_dir = './checkpoint/'
 
     train_path = '/home/khuong/Desktop/Projects/python/dataset/crowd_counting/SH_B/train/images'
@@ -40,16 +40,19 @@ def main():
     val_gt_path = '/home/khuong/Desktop/Projects/python/dataset/crowd_counting/SH_B/val/ground_truth'
 
     # last checkpoint
-    checkpointfile = './pre_train/checkpoint.74.pth.tar'
+    checkpointfile = os.path.join(checkpoint_dir, 'checkpoint.74.pth.tar')
 
     # some description
     method = 'mcnn'
     dataset_name = 'SH_B'
 
+    # log file
+    f_train_loss = open(os.path.join(log_dir, "train_loss.csv"), "a+")
+    f_val_loss = open(os.path.join(log_dir, "val_loss.csv"), "a+")
 
     # Training configuration
     start_epoch = 0
-    end_epoch = 77
+    end_epoch = 2000
     lr = 0.00001
     # momentum = 0.9
     disp_interval = 1000
@@ -98,7 +101,8 @@ def main():
     # training param
 
     if CONTINUE_TRAIN:
-        net, optimizer, start_epoch = utils.load_checkpoint(net, optimizer,filename=checkpointfile)
+        net, optimizer, start_epoch = utils.load_checkpoint(
+            net, optimizer, filename=checkpointfile)
 
     train_loss = 0
     step_cnt = 0
@@ -114,8 +118,7 @@ def main():
     best_mae = sys.maxsize
 
     # Start training
-    f_train_loss = open(os.path.join(log_dir,"train_loss.csv"),"w+")
-    f_val_loss = open(os.path.join(log_dir, "val_loss.csv"),"w+")
+
     for this_epoch in range(start_epoch, end_epoch-1):
         step = -1
         train_loss = 0
@@ -137,7 +140,8 @@ def main():
                 gt_count = np.sum(gt_data)
                 et_data = et_data.data.cpu().numpy()
                 et_count = np.sum(et_data)
-                utils.save_results(img_data, gt_data, et_data, output_dir, fname="{}.{}.png".format(this_epoch, step))
+                utils.save_results(img_data, gt_data, et_data, output_dir,
+                                   fname="{}.{}.png".format(this_epoch, step))
                 log_text = 'epoch: %4d, step %4d, Time: %.4fs, gt_cnt: %4.1f, et_cnt: %4.1f' % (this_epoch,
                                                                                                 step, 1./fps, gt_count, et_count)
                 log_print(log_text, color='green', attrs=['bold'])
@@ -146,41 +150,45 @@ def main():
             if re_cnt:
                 t.tic()
                 re_cnt = False
-        
+
         # Save checkpoint
         state = {'epoch': this_epoch + 1, 'state_dict': net.state_dict(),
-             'optimizer': optimizer.state_dict()}
-        cp_filename ="checkpoint.{}.pth.tar".format(this_epoch)
+                 'optimizer': optimizer.state_dict()}
+        cp_filename = "checkpoint.{}.pth.tar".format(this_epoch)
         torch.save(state, os.path.join(checkpoint_dir, cp_filename))
 # ========================== END 1 EPOCH==================================================================================
         train_mae, train_mse = evaluate_network(net, data_loader)
         f_train_loss.write("{},{}\n".format(train_mae, train_mse))
+        log_text = 'TRAINING - EPOCH: %d, MAE: %.1f, MSE: %0.1f' % (
+            this_epoch, train_mae, train_mse)
+        log_print(log_text, color='green', attrs=['bold'])
 # =====================================================VALIDATION=========================================================
         # calculate error on the validation dataset
         val_mae, val_mse = evaluate_network(net, data_loader_val)
         f_val_loss.write("{},{}\n".format(val_mae, val_mse))
-        log_text = 'EPOCH: %d, MAE: %.1f, MSE: %0.1f' % (this_epoch, val_mae, val_mse)
+        log_text = 'VALIDATION - EPOCH: %d, MAE: %.1f, MSE: %0.1f' % (
+            this_epoch, val_mae, val_mse)
         log_print(log_text, color='green', attrs=['bold'])
         # SAVE model
         is_save = False
         if val_mae <= best_mae:
             if val_mae < best_mae:
-                is_save=True
+                is_save = True
                 best_mae = val_mae
                 best_mse = val_mse
             else:
                 if val_mse < best_mse:
-                    is_save=True
+                    is_save = True
                     best_mse = val_mse
 
         if is_save:
-            save_name = os.path.join(output_dir, '{}_{}_{}.h5'.format(method, dataset_name, this_epoch))
+            save_name = os.path.join(output_dir, '{}_{}_{}.h5'.format(
+                method, dataset_name, this_epoch))
             network.save_net(save_name, net)
             best_model = '{}_{}_{}.h5'.format(method, dataset_name, this_epoch)
-            log_text = 'BEST MAE: %0.1f, BEST MSE: %0.1f, BEST MODEL: %s' % (best_mae, best_mse, best_model)
+            log_text = 'BEST MAE: %0.1f, BEST MSE: %0.1f, BEST MODEL: %s' % (
+                best_mae, best_mse, best_model)
             log_print(log_text, color='green', attrs=['bold'])
-
-
 
         # if use_tensorboard:
         #     exp.add_scalar_value('MAE', mae, step=epoch)
@@ -190,6 +198,7 @@ def main():
 
     f_train_loss.close()
     f_val_loss.close()
+
 
 if __name__ == "__main__":
     main()
